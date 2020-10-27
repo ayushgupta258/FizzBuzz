ec7cfbe4532f902b9fcbb95c956e27faaf6235da
ayushgupta258 <ayushgupta258@gmail.com>
2020-10-27 19:24:06 +0530

    Initialisation

---
+++ FizzBuzz.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for(int num=1; num<=100; num++){
+            System.out.println(""+num);
+        }
+    }
+}
