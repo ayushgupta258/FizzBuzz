19339fadc449bc1b3e23792d1af85ddf54f7b6ec
ayushgupta258 <ayushgupta258@gmail.com>
2020-10-27 19:27:24 +0530

    Print Buzz if number is multiple of 5

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         for(int num=1; num<=100; num++){
             if(num % 3 == 0)
                 System.out.println("Fizz");
-            else if(num == 5)
+            else if(num % 5 == 0)
                 System.out.println("Buzz");
             else
                 System.out.println(""+num);
