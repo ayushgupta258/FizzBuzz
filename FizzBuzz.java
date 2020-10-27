c8704944706d63dbb7e793283b0c2c32368537f2
ayushgupta258 <ayushgupta258@gmail.com>
2020-10-27 19:24:44 +0530

    Print Fizz if number is 3

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int num=1; num<=100; num++){
-            System.out.println(""+num);
+            if(num == 3)
+                System.out.println("Fizz");
+            else
+                System.out.println(""+num);
         }
     }
 }
