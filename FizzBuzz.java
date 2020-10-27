5adb2374b381411e7288be5772efdcd0dff079a4
ayushgupta258 <ayushgupta258@gmail.com>
2020-10-27 19:25:22 +0530

    Print Fizz if number is multiple of 3

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int num=1; num<=100; num++){
-            if(num == 3)
+            if(num % 3 == 0)
                 System.out.println("Fizz");
             else
                 System.out.println(""+num);
