e47635569f0dcd653d19e6e74058038562f4bd96
ayushgupta258 <ayushgupta258@gmail.com>
2020-10-27 19:29:07 +0530

    Print FizzBuzz if number is multiple of 3 & 5 both

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -7,6 +7,8 @@ public class FizzBuzz {
                 System.out.println("Fizz");
             else if(num % 5 == 0)
                 System.out.println("Buzz");
+            else if(num % 3 == 0 && num % 5 == 0)
+                System.out.println("FizzBuzz");
             else
                 System.out.println(""+num);
         }
